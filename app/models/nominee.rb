d896b544ab3eca1d15963217a023eff794ea3954
Ryan Kulp <ryanckulp@gmail.com>
2016-07-02 00:49:30 -0700

    update warrior status inline as before callback

--- app/models/nominee.rb
+++ app/models/nominee.rb
@@ -1,12 +1,13 @@
 class Nominee < ActiveRecord::Base
   belongs_to :nominator
 
-  after_save :promote_to_warrior?
+  before_save :promote_to_warrior?
 
   def promote_to_warrior?
     if self.votes == 5
       nominator = Nominator.find(self.nominator_id)
       warrior = Warrior.create(handle: self.handle, nominator_id: nominator.id)
+      self.warrior_status = true
       TwitterApi.update(".#{warrior.handle}, you're officially an SJW on http://www.sjwbot.com thanks to #{nominator.handle}")
     end
   end
--- app/models/warrior.rb
+++ app/models/warrior.rb
@@ -1,13 +1,4 @@
 class Warrior < ActiveRecord::Base
   belongs_to :nominator
 
-  # callbacks
-  after_create :set_nominee_status
-
-  def set_nominee_status
-    nominee = Nominee.find_by(handle: self.handle)
-    nominee.warrior_status = true
-    nominee.save
-  end
-
 end
